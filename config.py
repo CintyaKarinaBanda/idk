5ece31bb5ac37d42849784a40d79fd2768a63927
Karina Banda <karinabanda@MacBook-Pro-de-Karina.local>
2025-06-30 18:42:32 -0600

    first commit

--- config.py
+++ config.py
@@ -1,34 +1,30 @@
 # Configuración de AWS
 AWS_CONFIG = {
     "REGION": "us-east-1",
-    "ROLE_ARN": "arn:aws:iam::123456789012:role/EstafetaMonitoringRole",  # Reemplazar con el ARN del rol real
+    "ROLE_ARN": "arn:aws:iam::634576771855:role/ExtractData", 
     "ROLE_SESSION_NAME": "EstafetaMonitoringSession"
 }
 
-# Configuración de Gmail
 GMAIL_CONFIG = {
     "SCOPES": ['https://www.googleapis.com/auth/gmail.readonly'],
     "TOKEN_PATH": 'credentials/token.json',
     "CREDENTIALS_PATH": 'credentials/credentials.json'
 }
 
-# Configuración de correo electrónico
 EMAIL_CONFIG = {
     "REMITENTE": "karina.banda@xaldigital.com",
     "PASSWORD": "efci nhmi nzmj ipjk",
     "DESTINATARIO": "karina.banda@xaldigital.com",
     "COPIAS": []
-    # Descomentado: "COPIAS": ['benjamin.calvillo@xaldigital.com', "josue.jimenez@xaldigital.com", "alejandro.romero@xaldigital.com", "alejandro.calixto@xaldigital.com"]
+    # Descomentado: "COPIAS": ['infraestructura@xaldigital.com', 'automation-nextgen@xaldigital.com']
 }
 
-# Configuración de reportes
 REPORT_CONFIG = {
     "HORAS_CUSTOM": 12,
     "EXCEL_DIR": "excel",
     "DEFAULT_KEYWORD": "EST"
 }
 
-# Configuración de estilos para Excel
 EXCEL_STYLES = {
     "HEADER_COLOR": "4472C4",
     "HEADER_FONT_COLOR": "FFFFFF",
