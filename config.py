154d88307f6a023aa625897fca011a4d4e9ec769
Karina Banda <karinabanda@MacBook-Pro-de-Karina.local>
2025-07-03 10:14:18 -0600

    first commit

--- config.py
+++ config.py
@@ -14,9 +14,10 @@ GMAIL_CONFIG = {
 EMAIL_CONFIG = {
     "REMITENTE": "karina.banda@xaldigital.com",
     "PASSWORD": "efci nhmi nzmj ipjk",
-    "DESTINATARIO": "patricia.castillo@xaldigital.com",
-    #"COPIAS": []
-    "COPIAS": ['infraestructura@xaldigital.com', 'automation-nextgen@xaldigital.com']
+    "DESTINATARIO": "karina.banda@xaldigital.com",
+    #"DESTINATARIO": "patricia.castillo@xaldigital.com",
+    "COPIAS": []
+    #"COPIAS": ['infraestructura@xaldigital.com', 'automation-nextgen@xaldigital.com']
 }
 
 REPORT_CONFIG = {
